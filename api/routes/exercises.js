198a174b78c557a7021ffecde5e74ea18521bfac
Dimetriu <dima.rashidov@gmail.com>
2019-08-27 14:13:03 +0300

    finalize CRUD actions

--- api/routes/exercises.js
+++ api/routes/exercises.js
@@ -8,7 +8,7 @@ router.route('/').get((_, res) => {
     .catch(err => res.status(400).json(`Error: ${err}`))
 });
 
-/* POST create an Exercise */ 
+/* POST create an Exercise */
 router.route('/add').post((req, res) => {
   const { username, description } = req.body;
   const duration = Number(req.body.duration);
@@ -23,6 +23,36 @@ router.route('/add').post((req, res) => {
 
   newExercise.save()
     .then(() => res.json('Exercise added!'))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+/* GET the Exercise */
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+/* DELETE the Exercise */
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise deleted'))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+/* PUT update the Exercise */
+router.route('/update/:id').put((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = req.body.duration;
+      exercise.date = req.body.date;
+
+      exercise.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+    })
     .catch(err => res.status(400).json(`Error: ${err}`));
 });
 
